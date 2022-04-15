2aa348c4684c68f823ae34153c67b27ea52edbb5
bogdanaksenov <bogdanaks@bk.ru>
2022-04-15 16:11:25 +0300

    Update script

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -8,12 +8,9 @@ assert(process.env.ROUTER_ADDRESS, "ROUTER_ADDRESS not found");
 assert(process.env.FACTORY_ADDRESS, "FACTORY_ADDRESS not found");
 
 async function main() {
-  const Contract = await ethers.getContractFactory("AdapterUniswap");
+  const Contract = await ethers.getContractFactory("Token");
   const [owner] = await ethers.getSigners();
-  const contract = await Contract.deploy(
-    process.env.ROUTER_ADDRESS as string,
-    process.env.FACTORY_ADDRESS as string
-  );
+  const contract = await Contract.deploy("Academy Token", "ACDM");
 
   console.log("Contract deployed to:", contract.address);
   console.log("Owner address is: ", owner.address);
